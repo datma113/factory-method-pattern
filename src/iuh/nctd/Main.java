53585b0c58ce5469d1c7bd5a365e97667c22096c
datma <datma113112111@gmail.com>
2021-11-17 11:07:25 +0700

    done computer factory

--- src/iuh/nctd/Main.java
+++ src/iuh/nctd/Main.java
@@ -3,6 +3,9 @@ package iuh.nctd;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Computer pc = ComputerFactory.getComputer("pc","2 GB","500GB","2.4 GHz");
+        Computer server = ComputerFactory.getComputer("server","16  GB","1 TB","2.9 GHz");
+        System.out.println("Factory PC Config::"+pc);
+        System.out.println("Factory Server Config::"+server);
     }
 }
