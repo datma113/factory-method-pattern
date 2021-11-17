a30133164d1c8cdb71b3dfdc8f0b9d8f4537797b
datma <datma113112111@gmail.com>
2021-11-17 11:39:28 +0700

    done car factory

--- src/iuh/nctd/Main.java
+++ src/iuh/nctd/Main.java
@@ -7,5 +7,13 @@ public class Main {
         Computer server = ComputerFactory.getComputer("server","16  GB","1 TB","2.9 GHz");
         System.out.println("Factory PC Config::"+pc);
         System.out.println("Factory Server Config::"+server);
+
+
+        Car honda = CarFactory.getCar("Honda", "car honda", "none", "red", "JPN");
+        Car lexus = CarFactory.getCar("Lexus", "car lexxus", "none", "red", "JPN");
+        Car toyota = CarFactory.getCar("Toyota", "car toyota", "none", "red", "JPN");
+        System.out.println(honda);
+        System.out.println(lexus);
+        System.out.println(toyota);
     }
 }
