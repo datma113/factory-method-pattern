a441f3b22f64c07002e39ddbf7a3d6220ecc1cb1
datma <datma113112111@gmail.com>
2021-11-23 20:37:03 +0700

    done car factory

--- src/iuh/nctd/CPU.java
+++
@@ -1,4 +0,0 @@
-package iuh.nctd;
-
-public class CPU {
-}
--- src/iuh/nctd/Main.java
+++ src/iuh/nctd/Main.java
@@ -3,10 +3,10 @@ package iuh.nctd;
 public class Main {
 
     public static void main(String[] args) {
-        Computer pc = ComputerFactory.getComputer("pc","2 GB","500GB","2.4 GHz");
-        Computer server = ComputerFactory.getComputer("server","16  GB","1 TB","2.9 GHz");
-        System.out.println("Factory PC Config::"+pc);
-        System.out.println("Factory Server Config::"+server);
+        Computer pc = ComputerFactory.getComputer("pc", "2 GB", "500GB", "2.4 GHz");
+        Computer server = ComputerFactory.getComputer("server", "16  GB", "1 TB", "2.9 GHz");
+        System.out.println("Factory PC Config::" + pc);
+        System.out.println("Factory Server Config::" + server);
 
 
         Car honda = CarFactory.getCar("Honda", "car honda", "none", "red", "JPN");
@@ -14,6 +14,5 @@ public class Main {
         Car toyota = CarFactory.getCar("Toyota", "car toyota", "none", "red", "JPN");
         System.out.println(honda);
         System.out.println(lexus);
-        System.out.println(toyota);
     }
 }
